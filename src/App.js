d9c9fcdde4c01262ce4a677d8be02d8054ece1be
Garrett <garrett56x@gmail.com>
2020-10-01 16:04:43 -0700

    Using hooks to make API call to petfinder

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+// import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1 id="something-important">Adopt Me!</h1>
-      <Pet name="Bambu" animal="Dog" breed="California Mountain Dog" />
-      <Pet name="Teddy" animal="Dog" breed="Bichon Frise" />
-      <Pet name="Damon" animal="Cat" breed="Orange" />
+      <SearchParams />
     </div>
   );
 };
